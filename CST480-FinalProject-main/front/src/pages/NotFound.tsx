6064f8e26e92949767329a53cb4d752dc2735ed4
Ryan Gan <49723210+ryanzg123@users.noreply.github.com>
2023-04-09 15:34:02 -0400

    delete duplicate

--- CST480-FinalProject-main/front/src/pages/NotFound.tsx
+++
@@ -1,9 +0,0 @@
-import '../styles/CommonStyles.css'
-
-export default function NotFound() {
-    return (
-        <div className='defaultPageLayout'>
-            <h1>The page you are looking for does not exist.</h1>
-        </div>
-    );
-}
