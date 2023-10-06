fdaf24e30192516507bed51f40f97b966b21a1bb
Ryan Gan <ganzhiheng1@gmail.com>
2023-10-06 13:05:45 -0400

    Added more util functions to parse the roominfo

--- back/libs/utils.ts
+++ back/libs/utils.ts
@@ -1,13 +1,50 @@
 import { v4 as uuidv4 } from 'uuid';
+import {
+    DrawAndGuessDetailRoomInfo,
+    RoomInfoBase,
+    RoomStatus,
+} from '../models/types.js';
 
-function generateRoomId(): string {
+const generateRoomId = (): string => {
     return uuidv4();
-}
+};
 
-function getRandomInt(min: number, max: number) {
+const getRandomInt = (min: number, max: number) => {
     min = Math.ceil(min);
     max = Math.floor(max);
     return Math.floor(Math.random() * (max - min) + min); // The maximum is exclusive and the minimum is inclusive
-}
+};
 
-export { generateRoomId, getRandomInt };
+const getRoomStatus = (
+    currentSize: number,
+    maxSize: number,
+    isStarted: boolean = false,
+): RoomStatus => {
+    if (isStarted) {
+        return 'in progress';
+    }
+
+    return currentSize === maxSize ? 'full' : 'open';
+};
+
+const getDrawAndGuessLobbyRoomInfo = (
+    drawAndGuessDetailRoomInfo: DrawAndGuessDetailRoomInfo,
+): RoomInfoBase => {
+    return {
+        roomId: drawAndGuessDetailRoomInfo.roomId,
+        roomName: drawAndGuessDetailRoomInfo.roomName,
+        owner: drawAndGuessDetailRoomInfo.owner,
+        status: drawAndGuessDetailRoomInfo.status,
+        currentSize: drawAndGuessDetailRoomInfo.currentSize,
+        maxSize: drawAndGuessDetailRoomInfo.maxSize,
+        maxRound: drawAndGuessDetailRoomInfo.maxRound,
+        isPrivate: drawAndGuessDetailRoomInfo.isPrivate,
+    };
+};
+
+export {
+    generateRoomId,
+    getRandomInt,
+    getRoomStatus,
+    getDrawAndGuessLobbyRoomInfo,
+};
