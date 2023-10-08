9924ff62f21ac1f2940ff6e731e3f42c95fb1975
Ryan Gan <ganzhiheng1@gmail.com>
2023-10-08 16:38:17 -0400

    Fixed require socket validator, listens for disconnect event from backend to update in real time

--- front/src/components/require-socket.tsx
+++ front/src/components/require-socket.tsx
@@ -1,20 +1,31 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import toast from 'react-hot-toast';
 import { Navigate, Outlet } from 'react-router-dom';
 import { useSocket } from '../hooks/useSocket';
 
 const RequireSocket = () => {
     const { socket } = useSocket();
+    const [isConnected, setIsConnected] = useState(socket.connected);
 
     useEffect(() => {
-        if (!socket.connected) {
-            setTimeout(() => {
-                toast.error('Your connection to the server has been lost!');
-            }, 50);
-        }
-    }, [socket.connected]);
+        const handleConnect = () => setIsConnected(true);
+        const handleDisconnect = () => {
+            setIsConnected(false);
+            toast.error('Your connection to the server has been lost!');
+        };
 
-    return socket.connected ? <Outlet /> : <Navigate to="/Gamehub" replace />;
+        socket.on('connect', handleConnect);
+        socket.on('disconnect', handleDisconnect);
+
+        return () => {
+            // Cleanup event listeners
+            socket.off('connect', handleConnect);
+            socket.off('disconnect', handleDisconnect);
+        };
+    }, [socket]);
+
+    // If socket is not connected, redirect back to Gamehub page to try to initiate connection
+    return isConnected ? <Outlet /> : <Navigate to="/Gamehub" replace />;
 };
 
 export default RequireSocket;
